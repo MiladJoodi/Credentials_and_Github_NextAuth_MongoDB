c11db844334701881301fc2bf3a0d23cdb6d51d4
Milad Joodi <winbeta.com@gmail.com>
2024-03-18 16:42:33 +0330

    Next-Auth

---
+++ src/utils/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const connect = async ()=>{
+    try {
+        if (mongoose.connections[0].readyState){
+            return false;
+        }
+        await mongoose.connect(process.env.MONGODB_URI)
+        console.log("Connected!")
+    } catch (err){
+        console.log("Error => ", err);
+    }
+}
+
+export default connect;
